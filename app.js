a39a6907cb94d1323be0ce2c9fb6ecfd7ac1786f
grayweling <jy6228@yahoo.com>
2022-02-15 21:27:09 -0600

    added inquirer node

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const pageHtml = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
 
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//       console.log(profileDataArr[i]);
-//     }
-  
-//     console.log('================');
-  
-//     // Is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
 
-//   };
-// printProfileData(profileDataArgs);
-
-// console.log(profileDataArgs);
-
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
